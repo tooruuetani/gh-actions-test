3c6083b8e11ac81e7a73e6e49517adb17fb2d31a
tooru.uetani <tooru.uetani@mgw.ryoden.co.jp>
2024-04-08 12:02:07 +0900

    terraform plan 追加

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+terraform {
+  backend "s3" {
+    region  = "ap-northeast-1"
+    bucket  = "test-274-terraform-state-dev"
+    key     = "infrastructure.tfstate"
+    encrypt = true
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "now" {}
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.now.name
+  stage      = "dev"
+}
+
+resource "aws_ecr_repository" "main" {
+  name                 = "test-274-ecr-repository"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
