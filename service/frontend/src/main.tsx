b8e01985763186bc41ffc647b182a50c7352fec7
tooru.uetani <tooru.uetani@mgw.ryoden.co.jp>
2024-04-11 16:02:40 +0900

    fix

--- service/frontend/src/main.tsx
+++ service/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("VERSION:", import.meta.env.VITE_VERSION_SHA)
+console.log("VERSION:", import.meta.env.VITE_VERSION_SHA);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
