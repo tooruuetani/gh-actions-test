fb822cc05868ecf761c279efd1025554f8f558cf
tooru.uetani <tooru.uetani@mgw.ryoden.co.jp>
2024-04-11 12:25:30 +0900

    terraform test

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,10 @@ resource "aws_ecr_repository" "main" {
     scan_on_push = false
   }
 }
+
+output "repository_name" {
+  value = aws_ecr_repository.main.name
+}
+output "repository_url" {
+  value = aws_ecr_repository.main.repository_url
+}
